161f4c1d3bc76d29861ec3b9da90afcc3809c9cd
matheus kamers <mkamersandrade@gmail.com>
2024-03-31 17:46:09 -0300

    EX 5 - Correção

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 function alterar() {
-    let elements = document.getElementsByClassName('element')
+    let elements = document.getElementsByClassName('element');
 
-    for(let i=0; i < elements.length; i++) {
-        elements[i].classList.add('classe-alterada')
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.color = "blue";
+        elements[i].style.backgroundColor = "red";
     }
-
 }
