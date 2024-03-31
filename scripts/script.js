b318f0904831a25788f3859b61edfc1d9ae7529a
matheus kamers <mkamersandrade@gmail.com>
2024-03-31 17:50:20 -0300

    EX 6

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,4 @@
 function alterar() {
-    let elements = document.getElementsByClassName('element');
-
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.color = "blue";
-        elements[i].style.backgroundColor = "red";
-    }
+    let element = document.getElementById('element')
+    element.classList.add('element')
 }
