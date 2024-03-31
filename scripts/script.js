68ee32efab5b609b826775ae4e7d95544168f7d1
matheus kamers <mkamersandrade@gmail.com>
2024-03-31 18:43:35 -0300

    EX 9

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,9 @@ function alterar() {
     element.setAttribute('selected', 'selected')
 
 }
+
+function remover() {
+    let elem = document.querySelector('.op2')
+    let select = document.querySelector('.select')
+    elem.removeAttribute('selected', 'selected')
+}
