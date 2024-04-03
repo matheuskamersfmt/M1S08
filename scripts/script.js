34a7ba981bd9467d26ab049b7676adab04ea14f5
matheus kamers <matheus.kamers@checkstore.com.br>
2024-04-02 21:33:56 -0300

    EX 10

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,34 @@
-function alterar() {
-    let element = document.querySelector('.op2')
-    element.setAttribute('selected', 'selected')
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskText = taskInput.value.trim();
 
-}
+    if (taskText !== "") {
+        var taskList = document.getElementById("taskList");
 
-function remover() {
-    let elem = document.querySelector('.op2')
-    let select = document.querySelector('.select')
-    elem.removeAttribute('selected', 'selected')
+        var li = document.createElement("li");
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.addEventListener("change", function() {
+            li.style.textDecoration = checkbox.checked ? "line-through" : "none";
+        });
+
+        var label = document.createElement("label");
+        label.textContent = taskText;
+        label.classList.add('task-label')
+
+        var button = document.createElement("button");
+        button.textContent = "x";
+        button.classList.add('remove-button')
+        button.addEventListener("click", function() {
+            li.remove();
+        });
+
+        li.appendChild(checkbox);
+        li.appendChild(label);
+        li.appendChild(button);
+
+        taskList.appendChild(li);
+
+        taskInput.value = "";
+    }
 }
